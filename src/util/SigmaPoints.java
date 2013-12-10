c45c0aba954c17d2e72211fcccab63881c44065f
karnstrand <viktor.karnstrand@gmail.com>
2013-12-10 23:42:53 +0100

    Started to write sigma points class for unscented filter

---
+++ src/util/SigmaPoints.java
@@ -0,0 +1,32 @@
+package util; 
+
+import Jama.Matrix; 
+
+public class SigmaPoints {
+
+    private static final double lambda = 3; 
+    private final int dimesion; 
+    private final int numberOfPoints; 
+    private final double[] weights; 
+    private final Matrix[] points;  
+
+    public SigmaPoints(Gaussian density){
+
+	dimension = density.dimension; 
+	numberOfPoints = 2*dimension + 1; 
+	
+	points = new Matrix[numberOfPoints]; 
+	weights = new double[numberOfPoints]; 
+
+	points[0] = density.mean(); 
+
+	for(int i = 1; i < dimension; i++){
+	    
+	    
+
+	}
+
+    }
+
+
+}
