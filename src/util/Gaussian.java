64032fec95dd93710c44ede9c731e38463996743
karnstrand <viktor.karnstrand@gmail.com>
2013-12-15 23:25:50 +0100

    Now the code builds

--- src/util/Gaussian.java
+++ src/util/Gaussian.java
@@ -1,7 +1,30 @@
+package util; 
+
+import Jama.Matrix; 
+
 public class Gaussian{
 
-	public Matrix mean;
-	public Matrix cov;
+	private final Matrix x;
+	private final Matrix P;
+	private final int dim; 
+	
+	public Gaussian(Matrix mean, Matrix cov){
+		x = mean; 
+		P = cov; 
+		dim = mean.getRowDimension(); 
+	}
+	
+	public Matrix mean(){
+		return x; 
+	}
+	
+	public Matrix cov(){
+		return P; 
+	}
+	
+	public int dimension(){
+		return dim; 
+	}
 	
 	
 
--- src/util/SigmaPoints.java
+++ src/util/SigmaPoints.java
@@ -5,26 +5,26 @@ import Jama.Matrix;
 public class SigmaPoints {
 
     private static final double lambda = 3; 
-    private final int dimesion; 
+    private final int dimension; 
     private final int numberOfPoints; 
     private final double[] weights; 
     private final Matrix[] points;  
 
     public SigmaPoints(Gaussian density){
 
-	dimension = density.dimension; 
-	numberOfPoints = 2*dimension + 1; 
+		dimension = density.dimension(); 
+		numberOfPoints = 2*dimension + 1; 
 	
-	points = new Matrix[numberOfPoints]; 
-	weights = new double[numberOfPoints]; 
+		points = new Matrix[numberOfPoints]; 
+		weights = new double[numberOfPoints]; 
 
-	points[0] = density.mean(); 
+		points[0] = density.mean(); 
 
-	for(int i = 1; i < dimension; i++){
-	    
-	    
+		for(int i = 1; i < dimension; i++){
+		
+			points[i] = density.mean();
 
-	}
+		}
 
     }
 
