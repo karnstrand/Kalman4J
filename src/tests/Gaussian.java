7c64e3aa1f4081e757cb16388a7e4989961ad681
jaclea <jacob.e.leander@gmail.com>
2015-02-07 16:14:30 +0100

    Created test folder

---
+++ src/tests/Gaussian.java
@@ -0,0 +1,16 @@
+package util.probability; 
+
+import Jama.Matrix; 
+
+public class Gaussian{
+
+
+	// Test 1 - Create a matrix
+	
+	private final Matrix x;
+	private final Matrix P;
+	private final int dim; 
+	
+	
+
+}
